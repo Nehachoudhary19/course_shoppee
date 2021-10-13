dff70a41664017956f8a71c41f00f20e553e3fa7
Neha Choudhary <66562688+Nehachoudhary19@users.noreply.github.com>
2021-10-13 16:21:56 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,40 @@
+<html>
+  <head>
+    <title>Course Shoppee</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width ,initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="styles.css"/>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+          <div class="container">
+              <div class="navbar-header">
+                  <a href="index.php" class="navbar-brand" style="font-weight:bold">Course Shoppee</a>
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="sign_up.php"><span class="glyphicon glyphicon-user"></span> Signup
+                    </a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                    </li>
+					<li><a href="about_us.php"><span class="glyphicon glyphicon-th-list"></span> About Us  
+                    </a></li>
+					<li><a href="contact_us.php"><span class="glyphicon glyphicon-phone"></span> Contact Us
+                    </a></li>
+					
+                  </ul>
+              </div>
+          </div>         
+      </nav>
+
+  </body>
+   
+</html>
